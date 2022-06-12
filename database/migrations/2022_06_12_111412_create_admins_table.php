b5234f85455bbf8dfe50154ae944c6abf3a7794d
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2022-06-12 17:03:42 +0545

    created admin and vendor migration file, and model

---
+++ database/migrations/2022_06_12_111412_create_admins_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->integer('vendor_id');
+            $table->string('mobile');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('image');
+            $table->tinyInteger('status');   
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+};
